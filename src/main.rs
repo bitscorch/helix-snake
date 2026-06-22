53648892a477d1718248469b6e4ef12d7de255e9
bitscorch <paul@bitscorch.com>
2026-06-22 23:08:15 +0300

    add food

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::collections::VecDeque;
 
 use macroquad::{
-    color::{DARKPURPLE, YELLOW},
+    color::{BLUE, DARKPURPLE, YELLOW},
     input::{KeyCode, get_keys_down, get_last_key_pressed, is_key_down},
     main,
     math::{IVec2, Vec2, ivec2, vec2},
+    rand::gen_range,
     shapes::draw_rectangle,
     time::get_frame_time,
     window::{clear_background, next_frame, screen_height, screen_width},
@@ -22,6 +23,18 @@ fn cell_to_pixel(cell: IVec2, cell_size: Vec2) -> Vec2 {
     cell.as_vec2() * cell_size
 }
 
+fn spawn_food(body: &VecDeque<IVec2>) -> Option<IVec2> {
+    let free: Vec<IVec2> = (0..GRID_SIZE.x)
+        .flat_map(|x| (0..GRID_SIZE.y).map(move |y| ivec2(x, y)))
+        .filter(|cell| !body.contains(cell))
+        .collect();
+    if free.is_empty() {
+        None
+    } else {
+        Some(free[gen_range(0, free.len())])
+    }
+}
+
 #[main("Helix Snake")]
 async fn main() {
     let mut snake = Snake {
@@ -32,6 +45,9 @@ async fn main() {
         ]),
         dir: ivec2(1, 0),
     };
+    let mut food = spawn_food(&snake.body).unwrap();
+    let mut next_dir = snake.dir;
+
     let mut timer = 0.0;
 
     loop {
@@ -43,22 +59,28 @@ async fn main() {
         let dt = get_frame_time();
         timer += dt;
 
-        if timer >= STEP_TIME {
-            timer -= STEP_TIME;
-            snake.body.pop_back();
-            let new_head = *snake.body.front().unwrap() + snake.dir;
-            snake.body.push_front(new_head.rem_euclid(GRID_SIZE));
-        }
-
-        let new_dir = match get_last_key_pressed() {
+        let proposed_dir = match get_last_key_pressed() {
             Some(KeyCode::L) => ivec2(1, 0),
             Some(KeyCode::H) => ivec2(-1, 0),
             Some(KeyCode::J) => ivec2(0, 1),
             Some(KeyCode::K) => ivec2(0, -1),
-            _ => snake.dir,
+            _ => next_dir,
         };
-        if snake.dir.dot(new_dir) == 0 {
-            snake.dir = new_dir;
+
+        if proposed_dir != -snake.dir {
+            next_dir = proposed_dir;
+        }
+
+        if timer >= STEP_TIME {
+            timer -= STEP_TIME;
+            snake.dir = next_dir;
+            let new_head = (*snake.body.front().unwrap() + snake.dir).rem_euclid(GRID_SIZE);
+            snake.body.push_front(new_head);
+            if new_head == food {
+                food = spawn_food(&snake.body).expect("You win!");
+            } else {
+                snake.body.pop_back();
+            }
         }
 
         if is_key_down(KeyCode::Q) {
@@ -70,6 +92,9 @@ async fn main() {
             let pixel = cell_to_pixel(*part, cell_size);
             draw_rectangle(pixel.x, pixel.y, cell_size.x, cell_size.y, YELLOW);
         }
+
+        let pixel = cell_to_pixel(food, cell_size);
+        draw_rectangle(pixel.x, pixel.y, cell_size.x, cell_size.y, BLUE);
         next_frame().await
     }
 }
